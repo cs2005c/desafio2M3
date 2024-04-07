9585ce7c19648ce1e4b372da3be85e65408e134a
unknown <cs2005c@gmail.com>
2024-04-06 22:54:57 -0300

    Finalización del Desafio y se Entrega

--- assets/js/password.js
+++ assets/js/password.js
@@ -1,13 +1,22 @@
 let claveLiteral = "";
 let mensajeCorrecto = "";
+
+
 function evalua() {
 
 const passWord1 = document.getElementById('clave1').value;
 const passWord2 = document.getElementById('clave2').value;
 const passWord3 = document.getElementById('clave3').value;
 claveLiteral = passWord1+passWord2+passWord3;
-if (claveLiteral="911") {
-
+if (claveLiteral=="911") {
+      mensajeCorrecto="Password 1 Correcto";
+} else if (claveLiteral=="714") {
+    mensajeCorrecto="Password 2 Correcto";
+} else {
+        mensajeCorrecto= "Password Incorrecto";
 }
 
+const mensajetotal = document.getElementById('mensajeaviso');
+
+    mensajetotal.innerHTML=mensajeCorrecto;
 }
